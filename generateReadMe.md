58bfdccdd454376eb4b7635692d682e3189fae81
Darrell Carter <darrellcarter22@gmail.com>
2021-09-05 15:15:04 -0400

    updated input types

---
+++ generateReadMe.md
@@ -0,0 +1,11 @@
+ ## Project name: a
+ https://github.com/dcarter45/Professional-README-Generator
+ ## description: a
+ ## username: aa
+ ## email: a
+ ## license Type: 
+ ## install: a
+ ## test: 
+ ## usage: a
+ ## contributing: a
+ 
--- index.js
+++ index.js
@@ -1,51 +1,69 @@
+const handlebars = require('handlebars');
 const inquirer = require('inquirer');
-const fs= require('fs')
+const fs = require('fs')
 
 inquirer.prompt([
     {
         type: 'input',
-        messgae: 'What is your Github username',
-        name: 'Github Username'
-    },
-    {
-        type: 'input',
-        messgae: 'What is your email address',
-        name: 'email'
-    },
-    {
-        type: 'input',
-        messgae: 'What is your projects name',
-        name: 'project name'
+        message: 'What is your projects name?:',
+        name: 'projectName'
     }
     ,
     {
         type: 'input',
-        messgae: 'Please write a short description of your project',
+        message: 'Please write a short description of your project:',
         name: 'description'
     },
     {
         type: 'input',
-        messgae: 'What kind of licesnse should your project have',
-        name: 'license type'
+        message: 'What is your Github username?:',
+        name: 'githubUsername'
     },
     {
         type: 'input',
-        messgae: 'What command should you run to install dependencies',
+        message: 'What is your email address?:',
+        name: 'email'
+    },
+    {
+        type: 'input',
+        message: 'What kind of licesnse should your project have?:',
+        name: 'licenseType'
+    },
+    {
+        type: 'input',
+        message: 'What command should you run to install dependencies?:',
         name: 'install'
     },
     {
         type: 'input',
-        messgae: 'What command should be run to run tests',
+        message: 'What command should be run to run tests?:',
         name: ' test'
     },
     {
         type: 'input',
-        messgae: 'What does the user need to know about using the repo',
+        message: 'What does the user need to know about using the repo?:',
         name: 'usage'
     },
     {
         type: 'input',
-        messgae: 'What does the user need to know about contributing to the repo',
+        message: 'What does the user need to know about contributing to the repo?:',
         name: 'contributing'
     }
-])
+]).then((answers) => {
+    console.log(answers);
+
+    // Compile the source code
+    fs.readFile('README.handlebars', 'utf8', (err, templateText) => {
+        if (err) {
+            throw err;
+        }
+        console.log(typeof templateText);
+        const template = handlebars.compile(templateText);
+        let readmeText = template(answers);
+        console.log(readmeText);
+        fs.writeFile('generateReadMe.md', readmeText, (err) =>
+        err ? console.error(err) : console.log('Success!')
+        );
+    })
+
+})
